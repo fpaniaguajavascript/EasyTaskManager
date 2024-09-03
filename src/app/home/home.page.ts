56a9bed96767597611afdeb8f6783dc3b3a5946e
fpaniaguajavascript <fernando.paniagua.javascript@gmail.com>
2024-09-03 20:17:32 +0200

    Creación y borrado

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonList, IonLabel, IonItemSliding, IonAvatar, IonItemOptions, IonIcon, IonItemOption, IonBadge } from '@ionic/angular/standalone';
 import { TareasManagerService } from '../services/tareas-manager.service';
 import { CommonModule } from '@angular/common';
+import { addIcons } from "ionicons";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonBadge, IonItemOption, IonIcon, IonItemOptions, IonAvatar, IonItemSliding, IonLabel, IonList, IonButton, IonInput, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
+  imports: [FormsModule, IonBadge, IonItemOption, IonIcon, IonItemOptions, IonAvatar, IonItemSliding, IonLabel, IonList, IonButton, IonInput, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule],
 })
 export class HomePage {
+  public tarea: string = "";
   constructor(public servicioTareas: TareasManagerService) { }
+  crear() {
+    this.servicioTareas.create(this.tarea);
+  }
+  borrar(i: number) {
+    this.servicioTareas.delete(i);
+  }
 }
--- src/app/services/tareas-manager.service.ts
+++ src/app/services/tareas-manager.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TareasManagerService {
-  public tareas: string[] = ["Comprar pan", "Comprar vino", "Comprar abono tte"];
+  public tareas: string[] = ["Tarea prueba 1", "Tarea prueba 2"];
   constructor() { }
+  public create(tarea: string) {
+    this.tareas.push(tarea);
+  }
+  public delete(index: number) {
+    this.tareas.splice(index, 1);
+  }
 }
